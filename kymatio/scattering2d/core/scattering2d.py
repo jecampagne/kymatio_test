38d85dbc2511e092dd3675d808dbcd02375559e2
Jean-Eric Campagne <campagne@lal.in2p3.fr>
2023-03-21 19:25:23 +0100

    rename JEC outpout

--- kymatio/scattering2d/core/scattering2d.py
+++ kymatio/scattering2d/core/scattering2d.py
@@ -39,7 +39,7 @@ def scattering2d(x, pad, unpad, backend, J, L, phi, psi, max_order,
 
         #JEC
         U_1_c_sav = U_1_c.copy()
-        out_S_1_JEC.append({'coef':U_1_c_sav,
+        out_S_1_JEC.append({'coef_jec':U_1_c_sav,
                             'j': (j1,),
                             'n': (n1,),
                             'theta': (theta1,)
